949500fa5c2156dfade7a844a704c463859c43fe
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2018-04-02 16:02:06 -0400

    Defined module Demotion

--- src/Demotion.jl
+++ src/Demotion.jl
@@ -1,5 +1,8 @@
 
 __precompile__()
+
+module Demotion
+
 export demote
 
 demote(obj::Dict) = Dict((demote(k), demote(v)) for (k, v) in obj)
@@ -8,3 +11,5 @@ demote(obj::Set) = Set(demote(x) for x in obj)
 
 demote(obj::AbstractString) = obj
 demote(obj::Any) = obj
+
+end # module Demotion
