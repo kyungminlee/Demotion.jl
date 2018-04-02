5a95118d7f0d590786a6a8fae7260f926b7fbfa7
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2018-04-02 15:55:49 -0400

    Initial commit

---
+++ src/Demotion.jl
@@ -0,0 +1,12 @@
+
+__precompile__()
+export demote
+
+import Collections
+
+demote(obj::Dict) = Dict((demote(k), demote(v)) for (k, v) in obj)
+demote(obj::AbstractArray) = [demote(x) for x in obj]
+demote(obj::Set) = Set(demote(x) for x in obj)
+
+demote(obj::AbstractString) = obj
+demote(obj::Any) = obj
