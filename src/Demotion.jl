5d9014d41be7c135ac577a0e27ab58c8d7b5243b
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2018-04-02 15:59:25 -0400

    removed DataStructure dependency

--- src/Demotion.jl
+++ src/Demotion.jl
@@ -2,8 +2,6 @@
 __precompile__()
 export demote
 
-import DataStructures
-
 demote(obj::Dict) = Dict((demote(k), demote(v)) for (k, v) in obj)
 demote(obj::AbstractArray) = [demote(x) for x in obj]
 demote(obj::Set) = Set(demote(x) for x in obj)
